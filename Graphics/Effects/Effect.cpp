ae00bf119f5b77ab04ac5e0c6db3d853404cd447
Blackcatn13 <mdd_13@hotmail.com>
2015-02-01 22:36:36 +0100

    Added LighMap effect.

--- Graphics/Effects/Effect.cpp
+++ Graphics/Effects/Effect.cpp
@@ -9,92 +9,82 @@
 
 void CEffect::SetNullParameters()
 {
-	m_Effect = NULL;
-	
-	m_WorldMatrixParameter = NULL;
-	m_ViewMatrixParameter = NULL;
-	m_ProjectionMatrixParameter = NULL;
-	m_WorldViewMatrixParameter = NULL;
-	m_ViewProjectionMatrixParameter = NULL;
-	m_WorldViewProjectionMatrixParameter = NULL;
-	m_ViewToLightProjectionMatrixParameter = NULL;
-	m_LightEnabledParameter = NULL;
-	m_LightsTypeParameter = NULL;
-	m_LightsPositionParameter = NULL;
-	m_LightsDirectionParameter = NULL;
-	m_LightsAngleParameter = NULL;
-	m_LightsColorParameter = NULL;
-	m_LightsFallOffParameter = NULL;
-	m_LightsStartRangeAttenuationParameter = NULL;
-	m_LightsEndRangeAttenuationParameter = NULL;
-	m_LightsIntensityParameter = NULL;
-	m_CameraPositionParameter = NULL;
-	m_BonesParameter = NULL;
-	m_TimeParameter = NULL;
-	m_InverseWorldMatrixParameter = NULL;
-	m_InverseViewMatrixParameter = NULL;
-	m_InverseProjectionMatrixParameter = NULL;
-
-	////
-	m_UseShadowMaskTextureParameter = NULL;
-	m_UseStaticShadowmapParameter = NULL;
-	m_UseDynamicShadowmapParameter =NULL;
-		
-
+    m_Effect = NULL;
+    m_WorldMatrixParameter = NULL;
+    m_ViewMatrixParameter = NULL;
+    m_ProjectionMatrixParameter = NULL;
+    m_WorldViewMatrixParameter = NULL;
+    m_ViewProjectionMatrixParameter = NULL;
+    m_WorldViewProjectionMatrixParameter = NULL;
+    m_ViewToLightProjectionMatrixParameter = NULL;
+    m_LightEnabledParameter = NULL;
+    m_LightsTypeParameter = NULL;
+    m_LightsPositionParameter = NULL;
+    m_LightsDirectionParameter = NULL;
+    m_LightsAngleParameter = NULL;
+    m_LightsColorParameter = NULL;
+    m_LightsFallOffParameter = NULL;
+    m_LightsStartRangeAttenuationParameter = NULL;
+    m_LightsEndRangeAttenuationParameter = NULL;
+    m_LightsIntensityParameter = NULL;
+    m_CameraPositionParameter = NULL;
+    m_BonesParameter = NULL;
+    m_TimeParameter = NULL;
+    m_InverseWorldMatrixParameter = NULL;
+    m_InverseViewMatrixParameter = NULL;
+    m_InverseProjectionMatrixParameter = NULL;
+    ////
+    m_UseShadowMaskTextureParameter = NULL;
+    m_UseStaticShadowmapParameter = NULL;
+    m_UseDynamicShadowmapParameter = NULL;
 }
 
 void CEffect::GetParameterBySemantic(const std::string &SemanticName, D3DXHANDLE &l_Handle)
 {
-	l_Handle=m_Effect->GetParameterBySemantic(NULL,SemanticName.c_str());
-	if(l_Handle==NULL)
-		LOGGER->AddNewLog(ELL_ERROR, "Parameter by semantic '%s' wasn't found on effect '%s'", SemanticName.c_str(), m_FileName.c_str());
+    l_Handle = m_Effect->GetParameterBySemantic(NULL, SemanticName.c_str());
+    if (l_Handle == NULL)
+        LOGGER->AddNewLog(ELL_ERROR, "Parameter by semantic '%s' wasn't found on effect '%s'", SemanticName.c_str(), m_FileName.c_str());
 }
 bool CEffect::LoadEffect()
 {
-	SetNullParameters();
-	LPD3DXBUFFER l_ErrorBuffer=NULL;
-	HRESULT l_HR=D3DXCreateEffectFromFile(GRAPHM->GetDevice(), m_FileName.c_str(), NULL, NULL, D3DXSHADER_USE_LEGACY_D3DX9_31_DLL, NULL, &m_Effect, &l_ErrorBuffer);
-	if(l_ErrorBuffer)
-	{
-		LOGGER->AddNewLog(ELL_ERROR, "Error creating effect '%s':\n%s", m_FileName.c_str(), l_ErrorBuffer->GetBufferPointer());
-		CHECKED_RELEASE(l_ErrorBuffer);
-		return false;
-	}
-	
-	bool isOK = true;
-	
-	
-	
-	GetParameterBySemantic("WORLD", m_WorldMatrixParameter);
-	GetParameterBySemantic("VIEW", m_ViewMatrixParameter);
-	GetParameterBySemantic("PROJECTION", m_ProjectionMatrixParameter);
-	GetParameterBySemantic("INVERSEWORLD", m_InverseWorldMatrixParameter);
-	GetParameterBySemantic("INVERSEVIEW", m_InverseViewMatrixParameter);
-	GetParameterBySemantic("INVERSEPROJECTION", m_InverseProjectionMatrixParameter);
-	GetParameterBySemantic("WORLDVIEW", m_WorldViewMatrixParameter);
-	GetParameterBySemantic("VIEWPROJECTION", m_ViewProjectionMatrixParameter);
-	GetParameterBySemantic("WORLDVIEWPROJECTION", m_WorldViewProjectionMatrixParameter);
-	GetParameterBySemantic("VIEWTOLIGHTPROJECTION", m_ViewToLightProjectionMatrixParameter);
-	GetParameterBySemantic("LIGHTENABLED", m_LightEnabledParameter);
-	GetParameterBySemantic("LIGHTSTYPE", m_LightsTypeParameter);
-	GetParameterBySemantic("LIGHTSPOSITION", m_LightsPositionParameter);
-	GetParameterBySemantic("LIGHTSDIRECTION", m_LightsDirectionParameter);
-	GetParameterBySemantic("LIGHTSANGLE", m_LightsAngleParameter);
-	GetParameterBySemantic("LIGHTSCOLOR", m_LightsColorParameter);
-	GetParameterBySemantic("LIGHTSFALLOFF", m_LightsFallOffParameter);
-	GetParameterBySemantic("LIGHTSSTARTRANGEATTENUATION", m_LightsStartRangeAttenuationParameter);
-	GetParameterBySemantic("LIGHTSENDRANGEATTENUATION", m_LightsEndRangeAttenuationParameter);
-	GetParameterBySemantic("LIGHTSINTENSITY", m_LightsIntensityParameter);
-	GetParameterBySemantic("CAMERAPOSITION", m_CameraPositionParameter);
-	GetParameterBySemantic("BONES", m_BonesParameter);
-	GetParameterBySemantic("TIME", m_TimeParameter);
-	
-	return isOK;
+    SetNullParameters();
+    LPD3DXBUFFER l_ErrorBuffer = NULL;
+    HRESULT l_HR = D3DXCreateEffectFromFile(GRAPHM->GetDevice(), m_FileName.c_str(), NULL, NULL, D3DXSHADER_USE_LEGACY_D3DX9_31_DLL, NULL, &m_Effect, &l_ErrorBuffer);
+    if (l_ErrorBuffer) {
+        LOGGER->AddNewLog(ELL_ERROR, "Error creating effect '%s':\n%s", m_FileName.c_str(), l_ErrorBuffer->GetBufferPointer());
+        CHECKED_RELEASE(l_ErrorBuffer);
+        return false;
+    }
+    bool isOK = true;
+    GetParameterBySemantic("WORLD", m_WorldMatrixParameter);
+    GetParameterBySemantic("VIEW", m_ViewMatrixParameter);
+    GetParameterBySemantic("PROJECTION", m_ProjectionMatrixParameter);
+    GetParameterBySemantic("INVERSEWORLD", m_InverseWorldMatrixParameter);
+    GetParameterBySemantic("INVERSEVIEW", m_InverseViewMatrixParameter);
+    GetParameterBySemantic("INVERSEPROJECTION", m_InverseProjectionMatrixParameter);
+    GetParameterBySemantic("WORLDVIEW", m_WorldViewMatrixParameter);
+    GetParameterBySemantic("VIEWPROJECTION", m_ViewProjectionMatrixParameter);
+    GetParameterBySemantic("WORLDVIEWPROJECTION", m_WorldViewProjectionMatrixParameter);
+    GetParameterBySemantic("VIEWTOLIGHTPROJECTION", m_ViewToLightProjectionMatrixParameter);
+    GetParameterBySemantic("LIGHTENABLED", m_LightEnabledParameter);
+    GetParameterBySemantic("LIGHTSTYPE", m_LightsTypeParameter);
+    GetParameterBySemantic("LIGHTSPOSITION", m_LightsPositionParameter);
+    GetParameterBySemantic("LIGHTSDIRECTION", m_LightsDirectionParameter);
+    GetParameterBySemantic("LIGHTSANGLE", m_LightsAngleParameter);
+    GetParameterBySemantic("LIGHTSCOLOR", m_LightsColorParameter);
+    GetParameterBySemantic("LIGHTSFALLOFF", m_LightsFallOffParameter);
+    GetParameterBySemantic("LIGHTSSTARTRANGEATTENUATION", m_LightsStartRangeAttenuationParameter);
+    GetParameterBySemantic("LIGHTSENDRANGEATTENUATION", m_LightsEndRangeAttenuationParameter);
+    GetParameterBySemantic("LIGHTSINTENSITY", m_LightsIntensityParameter);
+    GetParameterBySemantic("CAMERAPOSITION", m_CameraPositionParameter);
+    GetParameterBySemantic("BONES", m_BonesParameter);
+    GetParameterBySemantic("TIME", m_TimeParameter);
+    return isOK;
 }
 void CEffect::Unload()
 {
-	CHECKED_RELEASE(m_Effect);
-	SetNullParameters();
+    CHECKED_RELEASE(m_Effect);
+    SetNullParameters();
 }
 
 CEffect::CEffect()
@@ -103,95 +93,85 @@ CEffect::CEffect()
 
 CEffect::~CEffect()
 {
-	Unload();
+    Unload();
 }
 
 bool CEffect::SetLights(size_t NumOfLights)
 {
-	CLightManager::TMapResource::iterator it = LIGHTM->GetResources().begin();
-	//std::map<std::string, CLight*>::iterator it = LIGHTM->GetResources().begin();
-	int l_lightIndex = 0;
-	std::map<std::string, CLight*> resources = LIGHTM->GetResources();
-	while(it != LIGHTM->GetResources().end() && l_lightIndex < NumOfLights)
-	{
-		m_LightsEnabled[l_lightIndex] = 1;
-		int l_type = it->second->GetType();
-		m_LightsType[l_lightIndex] = l_type;
-		float l_angle = 0;
-		if(l_type == CLight::TLightType::SPOT)
-		{
-			l_angle = ((CSpotLight*)it->second)->GetAngle();
-		}
-		m_LightsAngle[l_lightIndex] = l_angle;
-		float l_falloff = 0;
-		if(l_type == CLight::TLightType::SPOT)
-		{
-			l_falloff = ((CSpotLight*)it->second)->GetFallOff();
-		}
-		m_LightsFallOff[l_lightIndex] = l_falloff;
-		float l_startRangeAtten = it->second->GetStartRangeAttenuation();
-		m_LightsStartRangeAttenuation[l_lightIndex] = l_startRangeAtten;
-		float l_endRangeAtten = it->second->GetEndRangeAttenuation();
-		m_LightsEndRangeAttenuation[l_lightIndex] = l_endRangeAtten;
-		float l_intensity = it->second->GetIntensity();
-		m_LightsIntensity[l_lightIndex] = l_intensity;
-		Vect3f l_pos = it->second->GetPosition();
-		m_LightsPosition[l_lightIndex] = l_pos;
-		Vect3f l_direction;
-		if(l_type == CLight::TLightType::OMNI)
-		{
-			l_direction = Vect3f(0,0,0);
-		}else
-		{
-			l_direction = ((CDirectionalLight*)it->second)->GetDirection();
-			l_direction = l_direction.Normalize();
-		}
-		m_LightsDirection[l_lightIndex] = l_direction;
-		Vect3f l_color = Vect3f(it->second->GetColor().GetRed(),it->second->GetColor().GetGreen(), it->second->GetColor().GetBlue()) ;
-		m_LightsColor[l_lightIndex] = l_color;
-
-		it++;
-
-		l_lightIndex += 1;
-	}
-
-	m_Effect->SetIntArray(m_LightsTypeParameter, &m_LightsType[0], MAX_LIGHTS_BY_SHADER);
-	m_Effect->SetFloatArray(m_LightsAngleParameter, &m_LightsAngle[0], MAX_LIGHTS_BY_SHADER);
-	m_Effect->SetFloatArray(m_LightsFallOffParameter, &m_LightsFallOff[0], MAX_LIGHTS_BY_SHADER);
-	m_Effect->SetFloatArray(m_LightsIntensityParameter, &m_LightsIntensity[0], MAX_LIGHTS_BY_SHADER);
-	m_Effect->SetFloatArray(m_LightsStartRangeAttenuationParameter, &m_LightsStartRangeAttenuation[0], MAX_LIGHTS_BY_SHADER);
-	m_Effect->SetFloatArray(m_LightsEndRangeAttenuationParameter, &m_LightsEndRangeAttenuation[0], MAX_LIGHTS_BY_SHADER);
-	m_Effect->SetFloatArray(m_LightsPositionParameter, &m_LightsPosition[0].x, MAX_LIGHTS_BY_SHADER*3);
-	m_Effect->SetFloatArray(m_LightsDirectionParameter, &m_LightsDirection[0].x, MAX_LIGHTS_BY_SHADER*3);
-
-	return true;
+    CLightManager::TMapResource::iterator it = LIGHTM->GetResources().begin();
+    //std::map<std::string, CLight*>::iterator it = LIGHTM->GetResources().begin();
+    int l_lightIndex = 0;
+    std::map<std::string, CLight*> resources = LIGHTM->GetResources();
+    while (it != LIGHTM->GetResources().end() && l_lightIndex < NumOfLights) {
+        m_LightsEnabled[l_lightIndex] = 1;
+        int l_type = it->second->GetType();
+        m_LightsType[l_lightIndex] = l_type;
+        float l_angle = 0;
+        if (l_type == CLight::TLightType::SPOT) {
+            l_angle = ((CSpotLight*)it->second)->GetAngle();
+        }
+        m_LightsAngle[l_lightIndex] = l_angle;
+        float l_falloff = 0;
+        if (l_type == CLight::TLightType::SPOT) {
+            l_falloff = ((CSpotLight*)it->second)->GetFallOff();
+        }
+        m_LightsFallOff[l_lightIndex] = l_falloff;
+        float l_startRangeAtten = it->second->GetStartRangeAttenuation();
+        m_LightsStartRangeAttenuation[l_lightIndex] = l_startRangeAtten;
+        float l_endRangeAtten = it->second->GetEndRangeAttenuation();
+        m_LightsEndRangeAttenuation[l_lightIndex] = l_endRangeAtten;
+        float l_intensity = it->second->GetIntensity();
+        m_LightsIntensity[l_lightIndex] = l_intensity;
+        Vect3f l_pos = it->second->GetPosition();
+        m_LightsPosition[l_lightIndex] = l_pos;
+        Vect3f l_direction;
+        if (l_type == CLight::TLightType::OMNI) {
+            l_direction = Vect3f(0, 0, 0);
+        } else {
+            l_direction = ((CDirectionalLight*)it->second)->GetDirection();
+            l_direction = l_direction.Normalize();
+        }
+        m_LightsDirection[l_lightIndex] = l_direction;
+        Vect3f l_color = Vect3f(it->second->GetColor().GetRed(), it->second->GetColor().GetGreen(), it->second->GetColor().GetBlue()) ;
+        m_LightsColor[l_lightIndex] = l_color;
+        it++;
+        l_lightIndex += 1;
+    }
+    m_Effect->SetIntArray(m_LightsTypeParameter, &m_LightsType[0], MAX_LIGHTS_BY_SHADER);
+    m_Effect->SetFloatArray(m_LightsAngleParameter, &m_LightsAngle[0], MAX_LIGHTS_BY_SHADER);
+    m_Effect->SetFloatArray(m_LightsFallOffParameter, &m_LightsFallOff[0], MAX_LIGHTS_BY_SHADER);
+    m_Effect->SetFloatArray(m_LightsIntensityParameter, &m_LightsIntensity[0], MAX_LIGHTS_BY_SHADER);
+    m_Effect->SetFloatArray(m_LightsStartRangeAttenuationParameter, &m_LightsStartRangeAttenuation[0], MAX_LIGHTS_BY_SHADER);
+    m_Effect->SetFloatArray(m_LightsEndRangeAttenuationParameter, &m_LightsEndRangeAttenuation[0], MAX_LIGHTS_BY_SHADER);
+    m_Effect->SetFloatArray(m_LightsPositionParameter, &m_LightsPosition[0].x, MAX_LIGHTS_BY_SHADER * 3);
+    m_Effect->SetFloatArray(m_LightsDirectionParameter, &m_LightsDirection[0].x, MAX_LIGHTS_BY_SHADER * 3);
+    return true;
 }
 bool CEffect::Load(const std::string &FileName)
 {
-	m_FileName = FileName;
-	return LoadEffect();
-	
+    m_FileName = FileName;
+    return LoadEffect();
 }
 bool CEffect::Reload()
 {
-	Unload();
-	bool isOK = LoadEffect();
-	return isOK;
+    Unload();
+    bool isOK = LoadEffect();
+    return isOK;
 }
 //DirectX Methods Interface
 LPD3DXEFFECT CEffect::GetD3DEffect() const
 {
-	return m_Effect;
+    return m_Effect;
 }
 D3DXHANDLE CEffect::GetTechniqueByName(const std::string &TechniqueName)
 {
-	D3DXHANDLE l_EffectTechnique=m_Effect->GetTechniqueByName(TechniqueName.c_str());
-	return l_EffectTechnique;
+    D3DXHANDLE l_EffectTechnique = m_Effect->GetTechniqueByName(TechniqueName.c_str());
+    return l_EffectTechnique;
 }
 
 void CEffect::SetShadowMapParameters(bool UseShadowMaskTexture, bool UseStaticShadowmap, bool UseDynamicShadowmap)
 {
-	m_Effect->SetBool(m_UseShadowMaskTextureParameter, UseShadowMaskTexture ? TRUE : FALSE);
-	m_Effect->SetBool(m_UseStaticShadowmapParameter, UseStaticShadowmap ? TRUE : FALSE);
-	m_Effect->SetBool(m_UseDynamicShadowmapParameter, UseDynamicShadowmap ? TRUE : FALSE);
-}
+    m_Effect->SetBool(m_UseShadowMaskTextureParameter, UseShadowMaskTexture ? TRUE : FALSE);
+    m_Effect->SetBool(m_UseStaticShadowmapParameter, UseStaticShadowmap ? TRUE : FALSE);
+    m_Effect->SetBool(m_UseDynamicShadowmapParameter, UseDynamicShadowmap ? TRUE : FALSE);
+}
---
+++ bin/Videogame/Data/effects/LightMapEffect.fx
@@ -0,0 +1,100 @@
+#include "Globals.fxh"
+#include "Samplers.fxh"
+#include "VertexTypes.fxh"
+
+//DWORD  BCLR = 0xff202080;  // Background color (if no image)
+float3 g_LightAmbient=float3(0.2, 0.2, 0.2);
+float g_SpecularExponent = 100;
+
+struct PSVertex
+{
+    float4 HPosition	: POSITION;
+    float2 UV		: TEXCOORD0;
+    float2 UV2		: TEXCOORD1;
+	float3 WorldNormal: TEXCOORD2;
+    float3 WorldPosition: TEXCOORD3;
+};
+
+PSVertex RenderLightMapVS(
+    VertexVS_TTEXTURE2_NORMAL_VERTEX IN)
+{
+    PSVertex OUT = (PSVertex)0;
+	OUT.UV = IN.UV;
+	OUT.UV2 = IN.UV2;
+	OUT.HPosition = mul(float4 (IN.Position.xyz, 1.0),g_WorldViewProjectionMatrix);
+	OUT.WorldPosition=mul(float4(IN.Position.xyz, 1.0), g_WorldMatrix);
+	OUT.WorldNormal= normalize(mul(IN.Normal.xyz, (float3x3)g_WorldMatrix));
+	return OUT;
+}
+
+float4 RN20(PSVertex IN) : COLOR
+{
+	float3 l_Nn = normalize(IN.WorldNormal);
+	float3 l_DiffuseTex = tex2D(S0LinearWrapSampler, IN.UV).rgb;
+	float3 l_DiffuseLigthMap = tex2D(S1LinearWrapSampler, IN.UV2).rgb;
+	return float4(l_DiffuseLigthMap, 1);
+	float3 finalColor = l_DiffuseTex * l_DiffuseLigthMap;
+	float3 l_CameraPosition = g_InverseViewMatrix[3].xyz;
+	for(int i = 0; i < MAXLIGHTS; i++) 
+	{
+		if(g_LightEnabled[i] >= 0) 
+		{
+			
+			if(g_LightType[i]==0) //omni
+			{	
+				float3 l_DirVectorNoNormal = g_LightPosition[i]-IN.WorldPosition;
+				float l_distance = distance(g_LightPosition[i],IN.WorldPosition);
+				float l_Attenuation = 1-saturate((l_distance-g_NearAtten[i])/(g_FarAtten[i]-g_NearAtten[i]));
+				float3 l_DirVector = normalize(-l_DirVectorNoNormal);
+				float3 l_DiffuseContribution = saturate(dot(l_Nn,-l_DirVector));
+				float3 l_HV = normalize(normalize(l_CameraPosition - IN.WorldPosition)-l_DirVector);
+				float3 l_SpecularComponent = g_LightColor[i]*pow(saturate(dot(l_Nn,l_HV)), g_SpecularExponent);
+				finalColor = finalColor +((l_DiffuseTex.xyz*g_LightIntensity[i])*(l_DiffuseContribution*g_LightColor[i]*l_Attenuation) +l_SpecularComponent*g_LightIntensity[i]);
+			}
+			else if(g_LightType[i]==1) //directional
+			{
+				
+				float l_LightColor = saturate(dot(l_Nn, -g_LightDirection[i]));
+				float3 l_HV = normalize(normalize(l_CameraPosition - IN.WorldPosition)-g_LightDirection[i]);
+				float3 l_SpecularComponent = g_LightColor[i]*pow(saturate(dot(l_Nn,l_HV)), g_SpecularExponent);
+			
+				//finalColor = finalColor +(l_DiffuseTex.xyz*g_LightAmbient+l_DiffuseTex.xyz*l_LightColor*g_LightColor[i]*g_LightIntensity[i]+l_SpecularComponent*g_LightIntensity[i]);
+				finalColor = finalColor +(l_DiffuseTex.xyz*l_LightColor*g_LightColor[i]*g_LightIntensity[i]+l_SpecularComponent*g_LightIntensity[i]);
+			}
+			else if(g_LightType[i]==2) //spot
+			{
+				float l_distance = distance(g_LightPosition[i],IN.WorldPosition);
+				float l_Attenuation = 1-saturate((l_distance-g_NearAtten[i])/(g_FarAtten[i]-g_NearAtten[i]));
+								
+				float l_HalfFallOff = g_FallOff[i]/2.0;
+				float l_HalfAngle = g_LightAngle[i]/2.0;
+				float l_MaxAngle = l_HalfFallOff + l_HalfAngle;
+							
+				float l_AngAttenuation = saturate((g_LightDirection[i] - cos(l_HalfAngle))/(cos(l_HalfAngle+g_FallOff[i])-cos(l_HalfAngle)));
+
+				float l_LightColor = saturate(dot(l_Nn, -g_LightDirection[i]));
+				float3 l_HV = normalize(normalize(l_CameraPosition - IN.WorldPosition)-g_LightDirection[i]);
+				float3 l_SpecularComponent = g_LightColor[i]*pow(saturate(dot(l_Nn,l_HV)), g_SpecularExponent);
+				float3 l_DifuseContrib = l_DiffuseTex.xyz*l_LightColor*g_LightColor[i];
+
+				if(dot(l_Nn, -g_LightDirection[i]) >= cos(l_HalfAngle+g_FallOff[i]) ){
+					finalColor = finalColor +(l_DifuseContrib*g_LightIntensity[i]*l_AngAttenuation*l_Attenuation+l_SpecularComponent*g_LightIntensity[i]);
+				}
+					
+					
+				
+			}
+		}
+	}
+	return float4(finalColor, 1);
+}
+
+technique tec0
+{
+    pass p0
+    {
+		//CullMode = None;
+        VertexShader =compile vs_3_0 RenderLightMapVS();
+        PixelShader = compile ps_3_0 RN20();
+    }
+}
