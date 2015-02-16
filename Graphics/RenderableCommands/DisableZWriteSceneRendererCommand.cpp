0bed470f8f4adec91bc6d3d09a388b9d6c34368b
Cristian Ruiz <crisrnrn@gmail.com>
2015-02-16 15:41:54 +0100

    Enable y Disable ZtestCommand

--- Graphics/RenderableCommands/DisableZWriteSceneRendererCommand.cpp
+++ Graphics/RenderableCommands/DisableZWriteSceneRendererCommand.cpp
@@ -1,6 +1,7 @@
 #include "RenderableCommands\DisableZWriteSceneRendererCommand.h"
 
 CDisableZWriteSceneRendererCommand::CDisableZWriteSceneRendererCommand(CXMLTreeNode &atts) {}
+
 void CDisableZWriteSceneRendererCommand::Execute(CGraphicsManager &RM)
 {
     RM.GetDevice()->SetRenderState( D3DRS_ZWRITEENABLE, FALSE );
--- Graphics/RenderableCommands/DrawQuadRendererCommand.cpp
+++ Graphics/RenderableCommands/DrawQuadRendererCommand.cpp
@@ -1,1 +1,34 @@
 #include "RenderableCommands\BeginRenderSceneRendererCommand.h"
+
+void CDrawQuadRendererCommand::DrawColoredQuad2DTexturedInPixelsByEffectTechnique(CRenderManager *RM, CEffectTechnique *EffectTechnique, RECT Rect, CColor Color, CTexture *Texture,
+														float U0=0.0f, float V0=0.0f, float U1=1.0f, float V1=1.0f)
+{
+	EffectTechnique->BeginRender();
+	Máster en Creación de Videojuegos
+	LPD3DXEFFECT l_Effect=EffectTechnique->GetEffect()->GetD3DEffect();
+	if(l_Effect!=NULL)
+	{
+		l_Effect->SetTechnique(EffectTechnique->GetD3DTechnique());
+		UINT l_NumPasses;
+		l_Effect->Begin(&l_NumPasses, 0);
+		for (UINT iPass = 0; iPass < l_NumPasses; iPass++)
+		{
+			l_Effect->BeginPass(iPass);
+			RM->DrawColoredQuad2DTexturedInPixels(Rect,Color,Texture, U0,
+			V0, U1, V1);
+			l_Effect->EndPass();
+		}
+		l_Effect->End();
+	}
+}
+
+/*
+technique DrawQuadTechnique
+{
+	pass p0
+	{
+		AlphaBlendEnable = false;
+		CullMode = CCW;
+		PixelShader = compile ps_3_0 DrawQuadPS();
+	}
+}*/
