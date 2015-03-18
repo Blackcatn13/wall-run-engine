37e5d23b9df88580faba728cc1c62d7296006e8d
charlio85 <malakaimakaisson5@gmail.com>
2015-03-18 19:03:54 +0100

    dummy particles

---
+++ Graphics/Particles/dummy.cpp
@@ -0,0 +1,34 @@
+#include "MeshInstance.h"
+#include "Utils\Defines.h"
+#include "Core/Core.h"
+#include "XML\XMLTreeNode.h"
+#include "StaticMeshes\StaticMesh.h"
+#include "StaticMeshes\StaticMeshManager.h"
+#include "Core_Utils/MemLeaks.h"
+
+
+CMeshInstance::CMeshInstance(const std::string &Name, const std::string &CoreName)
+{
+    m_StaticMesh = SMESHM->GetResource(CoreName);
+    setName(Name);
+}
+
+CMeshInstance::CMeshInstance(const CXMLTreeNode &Node)
+{
+//TODO Cuando esté el código del Load que falta ponerlo aquí tb
+}
+CMeshInstance::~CMeshInstance()
+{
+    if (m_StaticMesh->isDestroyed())
+        m_StaticMesh = 0;
+}
+void CMeshInstance::Render(CGraphicsManager *RM)
+{
+    if (m_Printable) {
+        Mat44f t = m44fIDENTITY;
+        RM->SetTransform(t);
+        RM->SetTransform(getTransform());
+        m_StaticMesh->Render(RM);
+    }
+}
+
---
+++ Graphics/Particles/dummy.h
@@ -0,0 +1,23 @@
+#ifndef CMESHINSTANCE_H
+#define CMESHINSTANCE_H
+
+#include <string>
+#include "Renderable\RenderableObject.h"
+
+class CStaticMesh;
+class CGraphicsManager;
+class CXMLTreeNode;
+
+class CMeshInstance : public CRenderableObject
+{
+private:
+    CStaticMesh *m_StaticMesh;
+public:
+    CMeshInstance(const std::string &Name, const std::string &CoreName);
+    CMeshInstance(const CXMLTreeNode &Node);
+    ~CMeshInstance();
+    void Render(CGraphicsManager *RM);
+    void Update(float dt) {};
+};
+
+#endif
