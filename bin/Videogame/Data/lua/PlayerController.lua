5065b693de62ba07736e1273f772f746673709e9
malakaimakaisson5@gmail.com <malakaimakaisson5@gmail.com>
2015-03-29 17:14:31 +0200

    No vigneting, picky.y modified

--- bin/Videogame/Data/lua/PlayerController.lua
+++ bin/Videogame/Data/lua/PlayerController.lua
@@ -194,7 +194,8 @@ function move_character_controller_mesh(_player, _position)
 	local mesh = coreInstance:get_renderable_object_layer_manager():get_default_renderable_object_manager():get_resource("SpongePicky")
 	--coreInstance:trace(tostring(mesh:get_position().z))
 	mesh:set_yaw(_player:get_yaw())
-	mesh:set_position(_position)
+	local mesh_position = Vect3f(_position.x, _position.y-0.5, _position.z)
+	mesh:set_position(mesh_position)
 --[[CRenderableObject* malla = RENDLM->GetDefaultRenderableObjectManager()->GetResource("SpongePicky");
     malla->SetYaw(m_fYaw);
     malla->SetPosition(m_PhysicController->GetPosition());--]]
