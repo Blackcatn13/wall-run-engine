15a50a77f289b4bd9b4a978debcadd3e6df0fd28
Blackcatn13 <mdd_13@hotmail.com>
2015-03-29 17:28:35 +0200

    Added sala 0 in level 3.

--- Videogame/TestGameplay.cpp
+++ Videogame/TestGameplay.cpp
@@ -168,7 +168,8 @@ void CTestGameplay::Init() {
    CPhysicCookingMesh* m_CockMesh = new CPhysicCookingMesh();*/
   //m_CockMesh->Init(PHYSXM->GetPhysicsSDK(), m_Alloc);
 // PHYSXM->GetCookingMesh()->CreateMeshFromASE("./Data/VolFinal.ASE", "sceneTraining");
-  PHYSXM->GetCookingMesh()->CreateMeshFromASE("./Data/Level1/trainingPiky2.ASE", "sceneTraining");
+  //PHYSXM->GetCookingMesh()->CreateMeshFromASE("./Data/Level1/trainingPiky2.ASE", "sceneTraining");
+  PHYSXM->GetCookingMesh()->CreateMeshFromASE("./Data/Sala0.ASE", "sceneTraining");
 // PHYSXM->GetCookingMesh()->LoadFromXML("./Data/level1/LevelCooking.xml");
   //  m_CockMesh->CreateMeshFromASE("./Data/sceneTrainingPiky.ASE", "sceneTraining");
   m_PhysicActorCubeFix = new CPhysicActor(m_PhysicUserDataCube);
