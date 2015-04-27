8be0cb051247e003184e2cc1e6cab7d63c3c163f
Marc <mdd_13@hotmail.com>
2015-04-27 19:47:32 +0200

    Fixed reload of shaders not working.

--- Graphics/AnimatedModels/AnimatedModelManager.h
+++ Graphics/AnimatedModels/AnimatedModelManager.h
@@ -5,12 +5,11 @@
 
 class CAnimatedInstanceModel;
 
-class CAnimatedModelManager : public CMapManager<CAnimatedCoreModel>
-{
-public:
-    CAnimatedModelManager();
-    ~CAnimatedModelManager();
-    CAnimatedCoreModel * GetCore(const std::string &Name, const std::string &Path);
-    CAnimatedInstanceModel * GetInstance(const std::string &Name);
-    void Load(const std::string &Filename);
+class CAnimatedModelManager : public CMapManager<CAnimatedCoreModel> {
+ public:
+  CAnimatedModelManager();
+  ~CAnimatedModelManager();
+  CAnimatedCoreModel *GetCore(const std::string &Name, const std::string &Path);
+  CAnimatedInstanceModel *GetInstance(const std::string &Name);
+  void Load(const std::string &Filename);
 };
--- bin/Videogame/Data/lua/PlayerController.lua
+++ bin/Videogame/Data/lua/PlayerController.lua
@@ -1,6 +1,6 @@
 local coreInstance = CCoreLuaWrapper().m_CoreInstance;
 local is_init=true;
-local playerRenderable = coreInstance:get_renderable_object_layer_manager():get_default_renderable_object_manager():get_resource("SpongePicky");
+
 function on_update_player_lua(l_ElapsedTime)
 	--local coreInstance = CCoreLuaWrapper().m_CoreInstance;
 	local luaUtil = CCMathLuaUtils();
@@ -9,6 +9,7 @@ function on_update_player_lua(l_ElapsedTime)
 	local active_camera = cam_Controller:get_active_camera();
 	local camObject = active_camera.m_pObject3D;
 	local player = coreInstance:get_player_controller();
+	local playerRenderable = coreInstance:get_renderable_object_layer_manager():get_default_renderable_object_manager():get_resource("SpongePicky");
 	--////////////////////////////////////////////////////////
 	-- PARAMETERS
 	--////////////////////////////////////////////////////////
@@ -271,13 +272,13 @@ function on_update_player_lua(l_ElapsedTime)
 end
 
 function move_character_controller_mesh(_player, _position, _jumping)
-	local mesh = playerRenderable
+	local mesh = coreInstance:get_renderable_object_layer_manager():get_default_renderable_object_manager():get_resource("SpongePicky")
 	mesh:set_yaw(_player:get_yaw() + math.pi)
 	local pos;
 	if _jumping then
-		pos = playerRenderable:getAnimationBonePosition().y;
+		pos = mesh:getAnimationBonePosition().y;
 	else
-		pos = playerRenderable:getBonePosition().y;
+		pos = mesh:getBonePosition().y;
 	end
 	local mesh_position = Vect3f(_position.x, _position.y - pos - 0.2, _position.z)
 	mesh:set_position(mesh_position)
