5a0d868be23f77bb9f204aecb4be3b9c30bdf4b3
Cristian Ruiz <crisrnrn@gmail.com>
2015-05-23 18:41:48 +0200

    Arreglo enemigos, esaparecen bien al morir

--- bin/Videogame/Data/lua/MikMik.lua
+++ bin/Videogame/Data/lua/MikMik.lua
@@ -176,8 +176,9 @@ function mikmik_enter_dead(name)
 		local dead_pos = enemy.m_PhysicController:get_position()
 		coreInstance:trace(name);
 		dead_pos.y = dead_pos.y + 1000
-		enemy:set_position(dead_pos)
-		enemy.m_PhysicController:set_position(dead_pos)
+		--enemy:set_position(dead_pos)
+		--enemy.m_PhysicController:set_position(dead_pos)
+		enemy:move_to_position(dead_pos)
 	end
 end
 
