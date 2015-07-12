c829e13994a87da018ada2d917fcce0835d2e9d5
charlio85 <malakaimakaisson5@gmail.com>
2015-07-12 20:14:37 +0200

    Fixed checkpoint yaw

--- bin/Videogame/Data/lua/checkpoint.lua
+++ bin/Videogame/Data/lua/checkpoint.lua
@@ -8,12 +8,18 @@ function CheckPoint.new()
    -- setmetatable( new_inst, Player_mt ) -- all instances share the same metatable
 	self.is_activated = false
 	self.name = ""
+	--self.yaw = 0.0
 	------	 CHECKPOINT FUNCTIONS -----
 	function self.set_checkpoint(player, trigger_name)
 		local coreInstance = CCoreLuaWrapper().m_CoreInstance;
 		if self.is_activated == false then
-			local player_controller = player.get_player_controller()
-			self.last_position = player_controller:get_position()
+		
+			--player_controller.m_PhysicController:set_position(self.original_position)
+		--player_controller:set_yaw(self.original_yaw)
+		
+			--local player_controller = player.get_player_controller()
+			self.last_position = player_controller.m_PhysicController:get_position()
+			--self.yaw = player_controller:get_yaw()
 			coreInstance:trace("checkpoint con player position")
 			player.last_checkpoint = self
 			coreInstance:trace("last checkpoint set")
@@ -29,8 +35,19 @@ function CheckPoint.new()
 	function self.load_checkpoint(player)
 		local coreInstance = CCoreLuaWrapper().m_CoreInstance;
 			coreInstance:trace("activating checkpoint!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
-		local player_controller = player:get_player_controller()
-		player_controller:set_position(self.last_position)
+		--local player_controller = player:get_player_controller()
+		player_controller.m_PhysicController:set_position(self.last_position)
+		local active_camera = cam_Controller:get_active_camera()
+		local cam_object =  active_camera.m_pObject3D
+		local PlayerYaw =  - cam_object:get_yaw() + 1.57
+		if player_controller.m_is3D == true then
+			player_controller:set_yaw(PlayerYaw)
+		else
+			player_controller.m_isTurned = false
+			player_controller:set_yaw(PlayerYaw + 1.57); -- 90º
+		end
+		
+		--player_controller:set_yaw(self.yaw)
 	end
 	
 	return self
