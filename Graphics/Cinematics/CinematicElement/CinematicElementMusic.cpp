61708d4ccfe8666184a025f65e0f69412fe829a1
Marc <mdd_13@hotmail.com>
2015-10-26 17:26:21 +0100

    Added new cinematic element to play audio.

---
+++ Graphics/Cinematics/CinematicElement/CinematicElementMusic.cpp
@@ -0,0 +1,25 @@
+#include "CinematicElementMusic.h"
+#include "XML\XMLTreeNode.h"
+#include "Core\Core.h"
+#include "Cinematics\Cinematic.h"
+#include "WWSoundManager.h"
+#include "Core_Utils/MemLeaks.h"
+
+
+CCinematicElementMusic::CCinematicElementMusic(const CXMLTreeNode &node)
+  : CCinematicElement(node),
+    m_Event(node.GetPszISOProperty("name")),
+    m_Actor(node.GetPszISOProperty("actor")) {
+}
+
+bool CCinematicElementMusic::Execute() {
+  WWSNDMGR->PlayEvent(m_Event, m_Actor);
+  return true;
+}
+
+bool CCinematicElementMusic::Update(float dt) {
+  return false;
+}
+
+CCinematicElementMusic::~CCinematicElementMusic() {
+}
---
+++ Graphics/Cinematics/CinematicElement/CinematicElementMusic.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef CINEMATIC_ELEMENT_MUSIC_H
+#define CINEMATIC_ELEMENT_MUSIC_H
+
+#include "CinematicElement.h"
+#include <string>
+
+class CXMLTreeNode;
+class CCinematic;
+
+class CCinematicElementMusic : public CCinematicElement {
+ public:
+  CCinematicElementMusic(const CXMLTreeNode &node);
+  virtual ~CCinematicElementMusic();
+  bool Execute();
+  bool Update(float dt);
+ private:
+  bool m_played;
+  std::string m_Event;
+  std::string m_Actor;
+};
+
+
+#endif
